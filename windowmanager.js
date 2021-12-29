5125f58c5cb63a32f37b13cce730e501d40e25e3
Newg <g_robert@insa-toulouse.fr>
2021-12-29 16:15:16 +0100

    suppr test

--- windowmanager.js
+++ windowmanager.js
@@ -7,7 +7,6 @@ function param()
 {
     document.getElementById("set").style.display="inline"; 
     document.getElementById("interface").style.display="none";
-    console.log("Test");
 }
 
 
@@ -61,4 +60,3 @@ function addRow(){
     else {alert("Valeur non comprise entre 0 et 10! C'est la loi dsl")}
     
 }
-param()
